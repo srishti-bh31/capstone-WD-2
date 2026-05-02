9b643734362e1b502d84e737797a2df8d64e29fb
srishti-bh31 <srishtibh31@gmail.com>
2026-05-02 20:32:13 +0530

    Add conditional login link to Navbar
    Added login link conditionally based on login status.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 
 function Navbar() {
+  const isLoggedIn = localStorage.getItem("isLoggedIn");
+
   return (
     <div className="navbar">
       <h2>HealthCare</h2>
@@ -9,6 +11,8 @@ function Navbar() {
         <Link to="/">Home</Link>
         <Link to="/doctors">Doctors</Link>
         <Link to="/dashboard">Dashboard</Link>
+        
+        {!isLoggedIn && <Link to="/login">Login</Link>}
       </div>
     </div>
   );
